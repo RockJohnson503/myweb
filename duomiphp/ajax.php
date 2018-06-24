72188b49559078688524671af1fcdb9d35d4f135
RockJohnson503 <836867547@qq.com>
2018-06-24 16:40:36 +0800

    .

--- duomiphp/ajax.php
+++ duomiphp/ajax.php
@@ -72,6 +72,7 @@ function checkreg()
     $param = str_pad(mt_rand(0, 999999), 6, "0", STR_PAD_BOTH);
 
     $_SESSION["param"] = $param;
+    return send($email, $param);
 }
 
 function checkcaptcha(){
